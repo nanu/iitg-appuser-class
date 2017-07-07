c9ce5885d73f5de45f4fea51391ae82a37e32aea
Nanu Alan Kachari <nanu.kachari@iitg.ernet.in>
2017-07-07 18:09:06 +0530

    Update config.php

--- example-usage/config.php
+++ example-usage/config.php
@@ -6,17 +6,15 @@
  * 
  * Set the respective values as per your local requirements.
  *
- * Written by: Nanu Alan Kachari, Department of CSE, IIT Guwahati
- * on 30 June 2017
+ * Written by: Nanu Alan Kachari, Department of CSE, IIT Guwahati on 7 July 2017
  * 
- *
  */
 
 //turn on output buffering
 ob_start();
 
 //assign a name to this session, avoids collision between the $_SESSION keys
-session_name('iitgauthentication');
+session_name('popauthentication');
 
 //start new or resume existing session
 session_start();
@@ -27,7 +25,18 @@ date_default_timezone_set('Asia/Calcutta');
 //set your application title here
 define('APPTITLE','POP Authentication System');
 
+//set your application captcha image parameters here
+define('CAPTCHAWIDTH','250');
+define('CAPTCHAHEIGHT','70');
+define('CAPTCHACHARS','5');
+define('CAPTCHALINES','50');
+define('CAPTCHAFONTSIZE','50');
+define('CAPTCHAFONTANGLE','0');
+define('CAPTCHAFONT','fonts/xeroxmalfunction.ttf');
+
+//classes used by the application
 require('classes/iitg-appuser-class.php');
+require('classes/iitg-captcha-class.php');
 
 //instantiate the $IITGAppUser
 $iitgAppUser = new IITGAppUser;
